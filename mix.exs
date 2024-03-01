a9a181ebc09ef89455d02c20d6511c6f1f4c812e
Paolo Marino <paolo.marino@hey.com>
2024-03-01 22:53:32 +0100

    Install tailwind if missing

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule Makina.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["esbuild.install --if-missing"],
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind makina", "esbuild makina"],
       "assets.deploy": [
         "cmd npm install --prefix assets",
