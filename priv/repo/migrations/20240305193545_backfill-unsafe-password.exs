1b3aa87dabb62f519adb692397e2b0611ead9f21
Paolo Marino <paolo.marino@hey.com>
2024-03-05 22:21:45 +0100

    Keep schema snapshot

--- priv/repo/migrations/20240305193545_backfill-unsafe-password.exs
+++ priv/repo/migrations/20240305193545_backfill-unsafe-password.exs
@@ -1,6 +1,26 @@
+defmodule :"Elixir.Makina.Repo.Migrations.Backfill-unsafe-password.Service" do
+  use Ecto.Schema
+
+  schema "services" do
+    field(:name, :string)
+    field(:slug, :string)
+    field(:image_registry, :string, default: "hub.docker.com")
+    field(:is_private_registry, :boolean, default: false, virtual: true)
+    field(:image_registry_user, :string)
+    field(:image_registry_unsafe_password, :string)
+    field(:image_registry_password, :string, virtual: true, redact: true)
+    field(:image_registry_encrypted_password, :binary, redact: true)
+    field(:image_name, :string)
+    field(:image_tag, :string, default: "latest")
+    field(:expose_service, :boolean, default: false)
+
+    timestamps()
+  end
+end
+
 defmodule :"Elixir.Makina.Repo.Migrations.Backfill-unsafe-password" do
   use Ecto.Migration
-  alias Makina.Apps.Service
+  alias :"Elixir.Makina.Repo.Migrations.Backfill-unsafe-password.Service", as: Service
 
   import Ecto.Query
 
