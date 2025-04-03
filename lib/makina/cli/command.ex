0b82473953948d5898bc1cad1d14c72a0d50569b
Paolo Marino <code@pmarino.dev>
2025-04-03 19:58:42 +0200

    Include help message info within commands

--- lib/makina/cli/command.ex
+++ lib/makina/cli/command.ex
@@ -1,4 +1,7 @@
 defmodule Makina.Cli.Command do
   @callback exec(arguments :: list(), options :: list()) :: :ok | :error
   @callback options() :: list(keyword())
+
+  @callback name() :: String.t()
+  @callback short_description() :: String.t()
 end
