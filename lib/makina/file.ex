7d1f25ab98adc48dd3d7ceaaa04c2539d12512bf
Paolo Marino <code@pmarino.dev>
2025-04-03 16:28:17 +0200

    Extract servers information from makinafile

--- lib/makina/file.ex
+++ lib/makina/file.ex
@@ -1,7 +1,15 @@
 defmodule Makina.File do
+  alias Makina.Definitions.Server
+
   def compile_build_file(path) do
     {_term, bindings} = Code.eval_file(path)
 
     bindings
+  end
+
+  def fetch_servers(bindings) do
+    bindings
+    |> Keyword.values()
+    |> Enum.filter(&is_struct(&1, Server))
   end
 end
