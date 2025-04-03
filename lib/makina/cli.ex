1c3fe26de6881d541471d793ac9b9ad2836d50fe
Paolo Marino <code@pmarino.dev>
2025-04-03 14:01:49 +0200

    Add config setup

--- lib/makina/cli.ex
+++ lib/makina/cli.ex
@@ -1,7 +1,14 @@
 defmodule Makina.Cli do
+  def start(_, [:test]) do
+    {:ok, self()}
+  end
+
   def start(_, _args) do
-    IO.puts("HELLO")
+    dbg()
+    IO.puts("Hello")
 
     System.halt(0)
+
+    :ok
   end
 end
