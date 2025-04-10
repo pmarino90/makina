e80483c2e67f0f79e6f85a2d1b89845bd780d981
Paolo Marino <code@pmarino.dev>
2025-04-10 12:39:35 +0200

    Enable or disable HTTPS based on config
    Default is disables, as it requires additional configurations if one
    needs it enabled.

--- lib/makina/models/context.ex
+++ lib/makina/models/context.ex
@@ -1,10 +1,26 @@
 defmodule Makina.Models.Context do
-  @doc """
+  @moduledoc """
   Represents the context of a Makinafile
   """
-  defstruct id: nil, servers: [], standalone_applications: []
 
+  alias Makina.Models.Context
+  alias Makina.Models.ProxyConfig
+
+  defstruct id: nil, servers: [], standalone_applications: [], proxy_config: nil
+
+  @type t() :: %Context{
+          id: nonempty_binary(),
+          servers: list(),
+          standalone_applications: list(),
+          proxy_config: ProxyConfig.t()
+        }
+
+  @spec new(opts :: keyword()) :: Context.t()
   def new(opts) do
+    opts = Keyword.put(opts, :proxy_config, prepare_proxy_config(opts[:proxy_config]))
+
     struct(__MODULE__, opts)
   end
+
+  defp prepare_proxy_config(opts), do: ProxyConfig.new(opts)
 end
