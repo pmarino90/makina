9f4ef4efe9acc4116cb8a4871d590ee1ce373377
Paolo Marino <code@pmarino.dev>
2025-04-20 14:26:21 +0200

    Update only applications that changed when deploying new versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Makina.MixProject do
   def project do
     [
       app: :makina,
-      version: "0.1.17",
+      version: "0.1.18",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
